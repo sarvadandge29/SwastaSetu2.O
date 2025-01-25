5b52e49f7d322fe3cb9afd66a8820d89d47e42fc
shardul dhanokar <dhanokarshardul@gmail.com>
2025-01-25 19:24:42 +0530

    Question Answer Added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,14 @@
 import { Features } from "@/components/Features";
 import Hero from "@/components/Hero";
+import { AccordionDemo } from "@/components/Accordiondemo"; // Import the AccordionDemo component
 
 export default function Home() {
   return (
     <div className="overflow-auto scrollbar scrollbar-thumb-gray-500 scrollbar-track-gray-200">
       <Hero />
       <Features />
-      <div className="min-h-screen flex justify-center items-center border-2 border-black">
-        <h1 className="text-4xl font-bebas-neue mr-4">Home</h1>
+      <div className="my-8 mx-28">
+        <AccordionDemo />
       </div>
     </div>
   );
